d54e52d0354c577d920886fa72692f96951373e6
klaiberti <till.klaiber@esentri.com>
2019-04-15 16:33:31 +0100

    Configure Application Environment Variables

---
+++ src/main/java/io/pivotal/pal/tracker/EnvController.java
@@ -0,0 +1,33 @@
+package io.pivotal.pal.tracker;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class EnvController {
+
+
+    private Map<String, String> envMap;
+
+    public EnvController(
+            @Value("${port:NOT SET}") String port,
+            @Value("${memory.limit:NOT SET}") String memoryLimit,
+            @Value("${cf.instance.index:NOT SET}") String cfInstanceIndex,
+            @Value("${cf.instance.addr:NOT SET}") String cfInstanceAddr
+    ){
+        envMap = new HashMap<>();
+        envMap.put("PORT", port);
+        envMap.put("MEMORY_LIMIT", memoryLimit);
+        envMap.put("CF_INSTANCE_INDEX", cfInstanceIndex);
+        envMap.put("CF_INSTANCE_ADDR", cfInstanceAddr);
+    }
+
+    @GetMapping("/env")
+    public Map<String, String> getEnv() {
+        return envMap;
+    }
+}
