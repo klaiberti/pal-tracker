fbff5c80b521c3d3b17a2578062c2692a762e378
klaiberti <till.klaiber@esentri.com>
2019-04-17 11:37:46 +0100

    Database Migration

---
+++ databases/tracker/migrations/V1.1__extra_table.sql
@@ -0,0 +1,10 @@
+CREATE TABLE course_participants(
+  id        INT NOT NULL AUTO_INCREMENT,
+  name      VARCHAR(30),
+  age       INT
+
+  PRIMARY KEY(id)
+)
+
+  ENGINE = innodb
+  DEFAULT CHARSET = utf8;
